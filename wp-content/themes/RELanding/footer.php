ba6aa259c6870382717613feedb01265ba150f56
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-09 12:04:24 -0500

    re Landing

--- wp-content/themes/RELanding/footer.php
+++ wp-content/themes/RELanding/footer.php
@@ -4,13 +4,22 @@
     
 ?>
 
-<nav class="footerNav navbar navbar-expand-lg navbar-dark d-none d-sm-none d-md-none d-lg-block" style="background:rgba(0,0,0,0.4)">
+<nav class="footerNav navbar navbar-expand-lg navbar-dark d-none d-sm-none d-md-block d-lg-block" style="background:rgba(0,0,0,0.4)">
     <div class="navbar-nav fontSize22 millgoudy ">
-      
-      <button id="pics" class="nav-link btn fontSize22">Pictures</button>
-      <button id="amenities" class="nav-link btn fontSize22" >Amenities</button>      
-      <button id="pricing" class="nav-link btn fontSize22" >Pricing</button>
-      <button id="contact" class="nav-link btn fontSize22" >Contact</button>
+      <ul class="navbar-nav m-auto">
+        <li class="nav-item">
+          <button id="pics" class="nav-link btn fontSize22">Pictures</button>
+        </li>
+        <li class="nav-item ">
+          <button id="amenities" class="nav-link btn fontSize22" >Amenities</button>      
+        </li>
+        <li class="nav-item ">
+          <button id="pricing" class="nav-link btn fontSize22" >Pricing</button>
+        </li>
+        <li class="nav-item ">
+          <button id="contact" class="nav-link btn fontSize22" >Contact</button>
+        </li>
+      </ul>
     </div>
 </nav>
 </body>
