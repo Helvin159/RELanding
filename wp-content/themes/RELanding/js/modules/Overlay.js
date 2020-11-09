eb9b5db3706a7fcbd108ef0952801d0e573c0afb
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-09 11:02:18 -0500

    re Landing

--- wp-content/themes/RELanding/js/modules/Overlay.js
+++ wp-content/themes/RELanding/js/modules/Overlay.js
@@ -4,17 +4,23 @@ class overlay{
 
     // Constructor
 constructor(){
-
+    // const amenities = document.querySelector('#amenities');
+    this.amenities = $('#amenities');
+    // const amenitiesOverlay = document.querySelector('#amenitiesOverlay');
+    this.amenitiesOverlay = $('#amenitiesOverlay');
 }
 
     // Events
 events(){
-    
+    this.amenities.on("click", alert('hi'))
 }
 
 
     // Methods
-
+    overlayOn(){
+        this.amenitiesOverlay.addClass('overlayOn');
+        console.log('hi')
+    }
 }
 
 export default overlay;
