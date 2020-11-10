41f6272ee8904a65f3fd55f9cd8c235d92f7a6f7
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-09 21:41:26 -0500

    re Landing

--- wp-content/themes/RELanding/includes/hero.php
+++ wp-content/themes/RELanding/includes/hero.php
@@ -1,8 +1,8 @@
 <div class="container-fluid frontHero">
     <div class="container frontHeroText text-LEFT">
         <h1 class="millgoudy display-4 largeH1">150 CENTRAL PARK S</h1>
-        <h2 class="millgoudy smallH1">150 CENTRAL PARK S</h2>
-        <h2 class="millgoudy "><span>#604 <br></span>NEW YORK, NY 10019</h2>
+        <h1 class="millgoudy smallH1 text-center">150 <br> CENTRAL PARK S</h1>
+        <h2 class="millgoudy text-center "><span>#604 <br></span>NEW YORK, NY 10019</h2>
     </div>
 
 </div>
